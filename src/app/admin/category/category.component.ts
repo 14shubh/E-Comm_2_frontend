a6efa0dbce7760f687a4800418ddf9060ca441b9
shubham burghate <burghateshubham115@gmail.com>
2022-09-22 19:21:40 +0530

    add design to admin login page

--- src/app/admin/category/category.component.ts
+++ src/app/admin/category/category.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/model/category';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
+
+
 export class CategoryComponent implements OnInit {
-
+category:Category = new Category("","");
   constructor() { }
+  file:any ;
+  public AddCategory(){
+    console.log(this.category.categoryName);
+    console.log(this.category.categoryImage);
+  }
 
+  public select(event:any){
+    // console.log(event);
+    if(event.target.files.length > 0){
+      this.file = event.target.files[0];
+      this.category.categoryImage = this.file;
+    }
 
-
+    console.log(this.file.name);
+  }
   ngOnInit(): void {
   }
 
